3632055a19ae3a8336a5170e980fd6e94f1540dc
cheonsanghun <49335769+cheonsanghun@users.noreply.github.com>
2025-09-03 15:24:06 +0900

    메인 페이지 하단을 흰색 배경으로 분리하고 히어로 이미지 조정

--- matcha-talk-vue/src/views/Home.vue
+++ matcha-talk-vue/src/views/Home.vue
@@ -1,47 +1,61 @@
 <template>
-  <v-container class="py-10">
-    <div class="text-center">
-      <h1 class="text-h4 text-pink-darken-2 font-weight-bold">MatchaTalk</h1>
-      <div class="text-subtitle-1 text-pink-darken-1 mt-2">문화 교류 랜덤 채팅</div>
-      <v-img src="/src/assets/board_3.jpg" height="260" class="my-8 rounded-lg" cover />
-      <p class="text-body-2">
-        Matcha Talk에서 당신의 관심사와 취향이 맞는 특별한 인연을 찾아보세요.
-        벚꽃처럼 아름다운 만남이 기다리고 있습니다.
-      </p>
-      <h3 class="text-h6 mt-8 text-pink-darken-2 font-weight-bold">새로운 인연을 만나는 가장 아름다운 방법</h3>
-      <v-btn class="mt-4" color="pink" variant="elevated" size="large" :to="ctaTo">가입 하기</v-btn>
-    </div>
+  <div>
+    <section class="py-10 bg-pink-lighten-5">
+      <v-container>
+        <div class="text-center">
+          <h1 class="text-h3 text-pink-darken-2 font-weight-bold">MatchaTalk</h1>
+          <div class="text-subtitle-1 text-pink-darken-1 mt-2">문화 교류 랜덤 채팅</div>
+          <v-img
+            src="/src/assets/sakura-hero.png"
+            class="my-8 mx-auto"
+            height="240"
+            contain
+            style="max-width: 600px;"
+          />
+          <p class="text-body-2">
+            Matcha Talk에서 당신의 관심사와 취향이 맞는 특별한 인연을 찾아보세요.
+            벚꽃처럼 아름다운 만남이 기다리고 있습니다.
+          </p>
+          <h3 class="text-h6 mt-8 text-pink-darken-2 font-weight-bold">새로운 인연을 만나는 가장 아름다운 방법</h3>
+          <v-btn class="mt-4 rounded-pill px-8" color="pink" size="x-large" :to="ctaTo">가입 하기</v-btn>
+        </div>
+      </v-container>
+    </section>
 
-    <v-row class="mt-12" justify="center" align="stretch" no-gutters>
-      <v-col cols="12" sm="4" class="pa-4">
-        <v-card class="pa-6" variant="outlined" >
-          <div class="text-center">
-            <v-icon size="36" color="pink">mdi-account-multiple</v-icon>
-            <div class="text-subtitle-1 font-weight-bold mt-3">맞춤형 매칭</div>
-            <div class="text-body-2 mt-2">관심사와 취향을 분석하여 가장 맞는 인연을 소개합니다.</div>
-          </div>
-        </v-card>
-      </v-col>
-      <v-col cols="12" sm="4" class="pa-4">
-        <v-card class="pa-6" variant="outlined">
-          <div class="text-center">
-            <v-icon size="36" color="pink">mdi-chat-processing</v-icon>
-            <div class="text-subtitle-1 font-weight-bold mt-3">실시간 채팅</div>
-            <div class="text-body-2 mt-2">편안하고 안전한 환경에서 새로운 인연과 대화해보세요.</div>
-          </div>
-        </v-card>
-      </v-col>
-      <v-col cols="12" sm="4" class="pa-4">
-        <v-card class="pa-6" variant="outlined">
-          <div class="text-center">
-            <v-icon size="36" color="pink">mdi-gift</v-icon>
-            <div class="text-subtitle-1 font-weight-bold mt-3">특별한 선물</div>
-            <div class="text-body-2 mt-2">상점에서 특별 상품을 구매하여 마음을 전해보세요.</div>
-          </div>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+    <section class="bg-white">
+      <v-container class="py-10">
+        <v-row justify="center" align="stretch" no-gutters>
+          <v-col cols="12" sm="4" class="pa-4">
+            <v-card class="pa-6" elevation="2" rounded="lg">
+              <div class="text-center">
+                <v-icon size="36" color="pink">mdi-account-multiple</v-icon>
+                <div class="text-subtitle-1 font-weight-bold mt-3">맞춤형 매칭</div>
+                <div class="text-body-2 mt-2">관심사와 취향을 분석하여 가장 맞는 인연을 소개합니다.</div>
+              </div>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="4" class="pa-4">
+            <v-card class="pa-6" elevation="2" rounded="lg">
+              <div class="text-center">
+                <v-icon size="36" color="pink">mdi-chat-processing</v-icon>
+                <div class="text-subtitle-1 font-weight-bold mt-3">실시간 채팅</div>
+                <div class="text-body-2 mt-2">편안하고 안전한 환경에서 새로운 인연과 대화해보세요.</div>
+              </div>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="4" class="pa-4">
+            <v-card class="pa-6" elevation="2" rounded="lg">
+              <div class="text-center">
+                <v-icon size="36" color="pink">mdi-shield-check</v-icon>
+                <div class="text-subtitle-1 font-weight-bold mt-3">안전한 채팅</div>
+                <div class="text-body-2 mt-2">강력한 보안 시스템으로 개인정보를 보호합니다.</div>
+              </div>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </section>
+  </div>
 </template>
 
 <script setup>
