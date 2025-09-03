de979d9710cc23df5ae4f518e4c9ec93f696e886
cheonsanghun <mulchil123@naver.com>
2025-09-03 16:40:31 +0900

    update

--- matcha-talk-vue/src/components/AppHeader.vue
+++ matcha-talk-vue/src/components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-app-bar flat class="bg-white px-4">
-    <div class="d-flex align-center justify-space-between w-100">
+  <v-app-bar flat class="bg-white-lighten-5">
+    <v-container class="d-flex align-center justify-space-between">
       <div class="d-flex align-center">
         <v-icon class="me-2" color="pink">mdi-flower</v-icon>
         <router-link to="/" class="text-h6 text-decoration-none text-pink-darken-2">
@@ -10,7 +10,7 @@
 
       <div v-if="!isAuth" class="d-flex ga-3">
         <v-btn variant="outlined" color="pink" to="/login">로그인</v-btn>
-        <v-btn color="pink" class="text-white" to="/register">회원가입</v-btn>
+        <v-btn variant="outlined" color="pink" to="/register">회원가입</v-btn>
       </div>
 
       <div v-else class="d-flex ga-3">
@@ -19,13 +19,14 @@
         <v-btn variant="tonal" color="pink" to="/shop" disabled>상점</v-btn>
         <v-btn color="pink" to="/profile">프로필</v-btn>
       </div>
-    </div>
+    </v-container>
   </v-app-bar>
 </template>
 
 <script setup>
-import { storeToRefs } from 'pinia'
-import { useAuthStore } from '../stores/auth'
+import {storeToRefs} from 'pinia'
+import {useAuthStore} from '../stores/auth'
+
 const store = useAuthStore()
-const { isAuthenticated: isAuth } = storeToRefs(store)
+const {isAuthenticated: isAuth} = storeToRefs(store)
 </script>
--- matcha-talk-vue/src/views/Home.vue
+++ matcha-talk-vue/src/views/Home.vue
@@ -2,9 +2,9 @@
   <div>
     <div class="sakura-bg">
       <span
-        v-for="(p, i) in petals"
-        :key="i"
-        :style="{
+          v-for="(p, i) in petals"
+          :key="i"
+          :style="{
           left: p.left,
           animationDelay: p.delay,
           animationDuration: p.duration,
@@ -20,63 +20,78 @@
         <div class="text-center">
           <h1 class="text-h3 text-pink-darken-2 font-weight-bold">MatchaTalk</h1>
           <div class="text-subtitle-1 text-pink-darken-1 mt-2">문화 교류 랜덤 채팅</div>
-          <v-img
-            src="/src/assets/sakura-hero.png"
-            class="my-8 mx-auto"
-            height="240"
-            contain
-            style="max-width: 600px;"
-          />
+          <v-row justify="center" align="stretch" no-gutters>
+            <v-col cols="12" sm="4" class="pa-4">
+              <v-card class="pa-6" elevation="2" rounded="lg">
+                <v-img
+                    src="/src/assets/sakura-hero.png"
+                    class="my-8 mx-auto"
+                    height="240"
+                    contain
+                    style="max-width: 600px;"
+                />
+              </v-card>
+            </v-col>
+          </v-row>
           <p class="text-body-2">
             Matcha Talk에서 당신의 관심사와 취향이 맞는 특별한 인연을 찾아보세요.
             벚꽃처럼 아름다운 만남이 기다리고 있습니다.
           </p>
           <h3 class="text-h6 mt-8 text-pink-darken-2 font-weight-bold">새로운 인연을 만나는 가장 아름다운 방법</h3>
-          <v-btn class="mt-4 rounded-pill px-8" color="pink" size="x-large" :to="ctaTo">가입 하기</v-btn>
+          <div v-if="!isAuth">
+            <v-btn class="mt-4 rounded-pill px-8" color="pink" size="x-large" :to="ctaTo">가입 하기</v-btn>
+          </div>
+          <div v-if="isAuth">
+            <v-btn class="mt-4 rounded-pill px-8" color="pink" size="x-large" :to="ctaTo">매칭 시작</v-btn>
+          </div>
         </div>
       </v-container>
     </section>
 
-    <section class="bg-white">
-      <v-container class="py-10">
-        <v-row justify="center" align="stretch" no-gutters>
-          <v-col cols="12" sm="4" class="pa-4">
-            <v-card class="pa-6" elevation="2" rounded="lg">
-              <div class="text-center">
-                <v-icon size="36" color="pink">mdi-account-multiple</v-icon>
-                <div class="text-subtitle-1 font-weight-bold mt-3">맞춤형 매칭</div>
-                <div class="text-body-2 mt-2">관심사와 취향을 분석하여 가장 맞는 인연을 소개합니다.</div>
-              </div>
-            </v-card>
-          </v-col>
-          <v-col cols="12" sm="4" class="pa-4">
-            <v-card class="pa-6" elevation="2" rounded="lg">
-              <div class="text-center">
-                <v-icon size="36" color="pink">mdi-chat-processing</v-icon>
-                <div class="text-subtitle-1 font-weight-bold mt-3">실시간 채팅</div>
-                <div class="text-body-2 mt-2">편안하고 안전한 환경에서 새로운 인연과 대화해보세요.</div>
-              </div>
-            </v-card>
-          </v-col>
-          <v-col cols="12" sm="4" class="pa-4">
-            <v-card class="pa-6" elevation="2" rounded="lg">
-              <div class="text-center">
-                <v-icon size="36" color="pink">mdi-shield-check</v-icon>
-                <div class="text-subtitle-1 font-weight-bold mt-3">안전한 채팅</div>
-                <div class="text-body-2 mt-2">강력한 보안 시스템으로 개인정보를 보호합니다.</div>
-              </div>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </section>
+    <v-container class="py-10">
+      <v-row justify="center" align="stretch" no-gutters>
+        <v-col cols="12" sm="4" class="pa-4">
+          <v-card class="pa-6" elevation="2" rounded="lg">
+            <div class="text-center">
+              <v-icon size="36" color="pink">mdi-account-multiple</v-icon>
+              <div class="text-subtitle-1 font-weight-bold mt-3">맞춤형 매칭</div>
+              <div class="text-body-2 mt-2">관심사와 취향을 분석하여 가장 맞는 인연을 소개합니다.</div>
+            </div>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="4" class="pa-4">
+          <v-card class="pa-6" elevation="2" rounded="lg">
+            <div class="text-center">
+              <v-icon size="36" color="pink">mdi-chat-processing</v-icon>
+              <div class="text-subtitle-1 font-weight-bold mt-3">실시간 채팅</div>
+              <div class="text-body-2 mt-2">편안하고 안전한 환경에서 새로운 인연과 대화해보세요.</div>
+            </div>
+          </v-card>
+        </v-col>
+        <v-col cols="12" sm="4" class="pa-4">
+          <v-card class="pa-6" elevation="2" rounded="lg">
+            <div class="text-center">
+              <v-icon size="36" color="pink">mdi-shield-check</v-icon>
+              <div class="text-subtitle-1 font-weight-bold mt-3">안전한 채팅</div>
+              <div class="text-body-2 mt-2">강력한 보안 시스템으로 개인정보를 보호합니다.</div>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script setup>
+import {storeToRefs} from 'pinia'
+import {useAuthStore} from '../stores/auth'
+
+const store = useAuthStore()
+const {isAuthenticated: isAuth} = storeToRefs(store)
+
 const ctaTo = '/register'
 
-const petals = Array.from({ length: 20 }).map(() => ({
+const petals = Array.from({length: 20}).map(() => ({
   left: `${Math.random() * 100}%`,
   delay: `${-Math.random() * 20}s`,
   duration: `${10 + Math.random() * 10}s`,
@@ -84,6 +99,8 @@ const petals = Array.from({ length: 20 }).map(() => ({
   opacity: 0.5 + Math.random() * 0.5,
   move: Math.random() * 100 - 50,
 }))
+
+
 </script>
 
 <style scoped>
